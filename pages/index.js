c39f31c309bd94b9b1fb1a3448a38c76c6c4080f
ByCratingAnAccount <119974911+ByCratingAnAccount@users.noreply.github.com>
2022-12-29 01:46:30 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          zixman <a href="https://nextjs.org">Next.js!</a>
+          zxc<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
